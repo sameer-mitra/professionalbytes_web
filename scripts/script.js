22128f37f4a3948c47757741eb97b7f11c53efb3
sameer-mitra <49610563+sameer-mitra@users.noreply.github.com>
2019-10-23 21:40:57 +1000

    Added Files
    Added web files

---
+++ scripts/script.js
@@ -0,0 +1,16 @@
+/* ========================================== 
+scrollTop() >= 300
+Should be equal the the height of the header
+========================================== */
+
+$(window).scroll(function(){
+    if ($(window).scrollTop() >= 423) {
+        $('nav').addClass('fixed-header');
+        $('nav div').addClass('visible-title');
+    }
+    else {
+        $('nav').removeClass('fixed-header');
+        $('nav div').removeClass('visible-title');
+    }
+});
+
